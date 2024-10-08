1398342783af1b6ae75c690a9eae797ecf786de4
Evan Mitchell <30278906+evan-mitchell93@users.noreply.github.com>
2024-10-08 10:29:06 -0400

    Update dbconnect.py

--- dbconnect.py
+++ dbconnect.py
@@ -2,16 +2,18 @@ import pandas as pd
 import sqlalchemy
 from sqlalchemy import create_engine, text
 
-engine = create_engine('mssql+pymssql://admin:test123@192.168.11.253:1433/MitchellDB')
+ip = input("Enter: IP address\n")
+db = input("Enter: DB name\n")
+engine = create_engine(f'mssql+pymssql://admin:test123@{ip}:1433/{db}')
 
 conn = engine.connect()
 
 sql = "SELECT * FROM Users;"
 
-stmt = text("INSERT INTO Users (UserName,Passphrase,UserType,Active) VALUES ('test','test123','user','Yes')")
+stmt = text("INSERT INTO Users (UserName,Passphrase,UserType,Active) VALUES ('test4','test123','user','Yes')")
 conn.execute(stmt)
 conn.commit()
 
 
 df = pd.read_sql_query(sql,conn)
-print(df.head())
+print(df)
