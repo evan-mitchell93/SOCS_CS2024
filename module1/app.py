c3bf6bb6c575481e56c61fa95231c5744b2e11b6
Evan Mitchell <30278906+evan-mitchell93@users.noreply.github.com>
2024-10-11 12:18:12 -0400

    module 1 file

---
+++ module1/app.py
@@ -0,0 +1,32 @@
+#Evan Mitchell
+#Date Created: 10/11/2024
+#Last Edidted: 10/11/2024
+
+
+#Variables
+#Assignment and Delcaration
+x = 3
+y = 5
+
+firstname = "Evan"
+lastname = "Mitchell"
+
+float1 = 10.24
+
+#Display data to the console
+print(x)
+print(y)
+
+#String operators
+fullname = firstname + lastname
+
+print(fullname)
+
+fullname = firstname + " " + lastname
+print(fullname)
+
+first_doubled = firstname * 2
+print(first_doubled)
+
+fullname_doubled = (fullname + ",") * 2
+print(fullname_doubled)
