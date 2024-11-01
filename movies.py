2d89b4c6eee11eb5804ad3b7f735e3326636f59b
Evan Mitchell <30278906+evan-mitchell93@users.noreply.github.com>
2024-11-01 09:57:42 -0400

    Update movies.py

--- movies.py
+++ movies.py
@@ -2,5 +2,19 @@
 #10/31/2024
 #Determine if customer is old enough to purchase an R rated ticket.
 
-#Edit
 
+age = int(input("Enter your age: "))
+
+if age >= 18:
+    if age >= 65:
+        print("here is your discounted ticket")
+    else:
+        print("here is your ticket")
+else:
+    print("You cannot purchase a ticket")
+
+
+name = input("Enter your name: ")
+
+if name == "Jaden":
+    print("Loser")
